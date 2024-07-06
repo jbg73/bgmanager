2f7c69dbd700dbdd5f96bc302d393d1a80fe7662
jbg73 <javier.bascu00@gmail.com>
2024-07-06 12:47:31 +0200

    Initial commit

---
+++ src/main/java/com/boardgames_manager/bgmanager/BgmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.boardgames_manager.bgmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardgames_manager/bgmanager/BgmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgames_manager.bgmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
